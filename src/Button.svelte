edf73f29de4257acf47c5207e2931bc2a1147b3b
crisOspina523@gmail.com <crisospina523@gmail.com>
2020-03-01 21:38:40 -0500

    [add] component reusable svelte

---
+++ src/Button.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let name = ''
+	export let handleClick
+</script>
+
+<button on:click={handleClick}> {name} </button>
+
+<style>
+  button {
+		width: 100%;
+		height: 2rem;
+    padding: 0.2em;
+		background-color: beige;
+		border-radius: 1em;
+    cursor: pointer;
+		color: #000
+	}
+
+	button:hover {
+		width: 100%;
+		height: 2rem;
+    padding: 0.2em;
+		background-color: #ccc;
+		border-radius: 1em;
+    cursor: pointer;
+		color: #fff
+	}
+
+	@media (min-width: 640px) {
+			button {
+			width: 100%;
+			height: 2rem;
+			padding: 0.2em;
+			background-color: beige;
+			border-radius: 1em;
+			cursor: pointer;
+			color: #000
+		}
+
+		button:hover {
+			width: 100%;
+			height: 2rem;
+			padding: 0.2em;
+			background-color: #ccc;
+			border-radius: 1em;
+			cursor: pointer;
+			color: #fff
+		}
+	}
+</style>
